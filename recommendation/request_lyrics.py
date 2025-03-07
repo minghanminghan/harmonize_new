6792632b0af6cfec31c521e701678e872261c75c
minghanminghan <andrewjiang6789@gmail.com>
2025-03-07 00:16:02 -0500

    revised scraper

--- recommendation/request_lyrics.py
+++ recommendation/request_lyrics.py
@@ -1,65 +1,67 @@
 import os
+import csv
+import sys
 import time
 import asyncio
 import pandas as pd
 from syrics.api import Spotify
 
+
 async def main():
-    sp = Spotify("AQB0-7Ta_1hi63BM7CXSdhCugk_hOYP63j1WzPgqMlC3ILiPAz4iEVqZmoFw7tGbFRzsMGdd-NpSEJ8J8HNoneo7ncc7fITU6mm8aYxI0hsOTjnoqQO3SSfL1Wo0qwq0bBRPcMQAdAK1F0t-QAEBWF8uRKfdrmS_RvKxsTcbL6s4M-5rhrQbum-zaacFOtXz4qTV3kPYGdXX23E22nw")
+    # read ./data/cookies
+    with open('./data/cookies.txt', 'r') as file:
+        cookies = [i.strip() for i in file.readlines()]
+        workers = [Spotify(i) for i in cookies]
+
+    # read from ./data/api_lyrics.txt what track_id's have already been read
+
+    num_workers: int = len(workers)
+    print('# workers:',num_workers)
+
+    failures: list[int] = [0]*num_workers
+    counter: list[int] = [0]*num_workers
+    reset_thres: list[int] = [0]*num_workers # if failed 10 requests in a row, pause for 2 minutes
+    reset_interval: int = 120
+    interval: int = 5
 
     df = pd.read_csv('./data/data_active.csv', index_col=0)
-    q: list[str] = df['track_id'].to_list()
-    lyrics = [] # list of strings
+    q: list[str] = df['track_id'].to_list()[:100]
+    print('queue length:',len(q))
 
-    #print(len(q))
-
-    q_mini: list[str] = q[:100]
-    failures = 0
+    start = time.time()
     counter = 0
-    break_thres = 20
-    interval = 5
-    reset_interval = 120
-
-    with open('./data/api_lyrics.txt', 'w', encoding='utf-8') as lyrics, open('./data/retries.txt', 'w', encoding='utf-8') as retry:
-        while q_mini:
-            cur = q_mini.pop()
-            res = sp.get_lyrics(cur)
+    with open('./data/api_lyrics.csv', 'w', encoding='utf-8') as lyrics, open('./data/retries.txt', 'w', encoding='utf-8') as retry:
+        lyrics_writer = csv.writer(lyrics, delimiter=', ')
+        while q:
+            index = counter % num_workers
+            track_id = q.pop()
+            res = workers[index].get_lyrics(track_id)
             local = []
-            # print(counter, cur)
+            
+            print(f'{counter} (worker: {index}, track_id: {track_id})')
 
-            if res == None: # failed request OR lyrics don't exist
-                print('failed:', counter, cur)
-                failures += 1
-                break_thres -= 1
-                if break_thres <= 0: # 20 failed requests in 25 attempts
+            if res == None: # failed request or lyrics don't exist
+                print('failed')
+                failures[index] += 1
+                reset_thres[index] += 1
+                if reset_thres[index] == 10: # 10 failed requests in a row
                     print(f'passed break threshold. sleeping {reset_interval}')
                     await asyncio.sleep(reset_interval)
-                    break_thres = 5
-                retry.write(cur+'\n')
+                    reset_thres = 0
+                retry.write(track_id+'\n')
 
             else:
+                reset_thres[index] = 0 # reset
                 for line in res['lyrics']['lines']:
                     local.append(line['words'])
             
-            lyrics.write(' '.join(local)+'\n')
-            
+            lyrics_writer.writerow([track_id, ' '.join(local)+'\n'])
+
             counter += 1
-            await asyncio.sleep(interval)
-            if counter % 25 == 0:
-                print(f'requests made: {counter}, failures: {failures}')
-                break_thres = 20
-                # print(f'reached rate limit. sleeping {interval}')
-                # await asyncio.sleep(interval)
+            if counter % num_workers == 0: # finished looping, pause
+                await asyncio.sleep(interval) # still not sure about this
 
-    # # write lyrics
-    # df['lyrics'] = lyrics
-    # df.to_csv('/data/data_active_lyrics', encoding='utf-8')
-
-    # write retries
-    # with open('retries', 'w') as file:
-    #     for id in retry:
-    #         file.write(id)
-    #         file.write('\n')
-
+    print(f'processed {sum(counter)} lyrics in {(time.time()-start)*1000}s with {sum(failures)} failures')
+    
 if __name__ == '__main__':
     asyncio.run(main())
